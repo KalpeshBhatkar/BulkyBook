5987f340d12e20398e9dbcfb34fc1b2c1f4a4724
Kalpesh Bhatkar <kalpeshbhatkar@outlook.com>
2026-07-23 16:30:55 +0530

    Toastr notification implemented using TempData and partial view

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -36,6 +36,7 @@ namespace BulkyBookWeb.Controllers
             {
                 _context.Categories.Add(category);
                 _context.SaveChanges();
+                TempData["success"] = "Category created successfully.";
                 return RedirectToAction("Index");
             }
             return View();
@@ -68,6 +69,7 @@ namespace BulkyBookWeb.Controllers
             {
                 _context.Categories.Update(category);
                 _context.SaveChanges();
+                TempData["success"] = "Category updated successfully.";
                 return RedirectToAction("Index");
             }
             return View();
@@ -100,6 +102,7 @@ namespace BulkyBookWeb.Controllers
             }
             _context.Categories.Remove(category);
             _context.SaveChanges();
+            TempData["success"] = "Category deleted successfully.";
             return RedirectToAction("Index");
         }
     }
