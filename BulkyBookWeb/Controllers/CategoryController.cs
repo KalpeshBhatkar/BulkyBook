82f1f5b9a3f5660602964894d70af024f4b65f06
Kalpesh Bhatkar <kalpeshbhatkar@outlook.com>
2026-07-23 12:17:29 +0530

    DisplayOrder Column Added in Category table and Migration done

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        public CategoryController()
+        {
+            
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
