d6188546029d68aa72b34a6d17367e72f0e06623
Kalpesh Bhatkar <kalpeshbhatkar916@gmail.com>
2026-07-25 11:45:05 +0530

    Product Table Added and Data Seed also done

---
+++ BulkyBook.Models/Product.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace BulkyBook.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public string ISBN { get; set; } = string.Empty;
+
+        [Required]
+        public string Author { get; set; } = string.Empty;
+
+        [Required]
+        [DisplayName("List Price")]
+        [Range(1, 1000, ErrorMessage = "List Price must be between 1 and 10000.")]
+        public double ListPrice { get; set; }
+
+        [Required]
+        [DisplayName("Price for 1-50")]
+        [Range(1, 1000, ErrorMessage = "Price must be between 1 and 1000.")]
+        public double Price { get; set; }
+
+        [Required]
+        [DisplayName("Price for 50+")]
+        [Range(1, 1000, ErrorMessage = "Price must be between 1 and 1000.")]
+        public double Price50 { get; set; }
+
+        [Required]
+        [DisplayName("Price for 100+")]
+        [Range(1, 1000, ErrorMessage = "Price must be between 1 and 1000.")]
+        public double Price100 { get; set; }
+
+        [DisplayName("Product Image+")]
+        [ValidateNever]
+        public string? ImageUrl { get; set; }
+
+        public int CategoryId { get; set; }
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; }
+    }
+}
