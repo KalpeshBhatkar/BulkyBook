567176b329c42aa78e3ed6d291ad5e3eb213ab3b
Kalpesh Bhatkar <kalpeshbhatkar916@gmail.com>
2026-08-01 11:04:28 +0530

    Multiple Navigation

---
+++ BulkyBookWeb/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using BulkyBook.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.RoleAdmin)]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
