f0d174763fc9bc795e43bbff523b5b7fbb2a4177
Kalpesh Bhatkar <kalpeshbhatkar916@gmail.com>
2026-08-04 18:26:43 +0530

    Order View added for Admin Portal

---
+++ BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,39 @@
+﻿using BulkyBook.Business.Services.IServices;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.RoleAdmin)]
+    public class OrderController : Controller
+    {
+        private readonly IOrderService _orderService;
+        private readonly ICategoryService _categoryService;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        #region API Calls
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetAll()
+        {
+            var orders = await _orderService.GetAllOrderAsync();
+            return Json(new { data = orders });
+        }
+
+        #endregion
+    }
+}
---
+++ BulkyBookWeb/wwwroot/js/orders.js
@@ -0,0 +1,68 @@
+﻿var orderDataTable;
+
+$(document).ready(function () {
+    orderDataTable();
+});
+
+//$(document).ready(function () {
+orderDataTable = $('#tblData').DataTable({
+    ajax: {
+        url: '/admin/order/getall',
+        type: 'GET'
+    },
+    columns: [
+        { data: 'id', width: "5%" },
+        { data: 'name', width: "20%" },
+
+        { data: 'phoneNumber', width: "15%" },
+        { data: 'applicationUser.email', width: "20%" },
+        {
+            data: 'orderStatus', width: "15%"
+            , render: function (data) { return '<span class="badge bg-secondary">' + data + '</span>'; }
+        },
+        {
+            data: 'orderTotal', width: "15%"
+            , render: function (data) { return '$' + data.toFixed(2); }
+        },
+        {
+            data: 'id', width: "10%"
+            , render: function (data) {
+                return `<div class="d-flex gap-2 justify-conetent-end">
+                    <a href="/admin/order/details?orderId=${data}" class="btn btn-sm btn-outline-success">
+                        <i class="bi bi-pencil-square"></i> Details
+                    </a>
+                </div>`;
+            }
+        }
+    ]
+});
+//});
+
+function Delete(url) {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    productDataTable.ajax.reload();
+
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Your file has been deleted.",
+                        icon: "success"
+                    });
+
+                }
+            });
+        }            
+    });
+}
