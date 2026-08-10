e70af0dceff689acf5a4e018b77e07489d695626
Kalpesh Bhatkar <kalpeshbhatkar916@gmail.com>
2026-08-10 23:22:40 +0530

    Admin Dashboard Count and Chart Implemented

---
+++ BulkyBook.Models/ViewModels/DashboardVM.cs
@@ -0,0 +1,11 @@
+﻿
+namespace BulkyBook.Models.ViewModels
+{
+    public class DashboardVM
+    {
+        public int TotalOrders { get; set; }
+        public int TotalProducts { get; set; }
+        public int TotalUsers { get; set; }
+        public double TotalRevenue { get; set; }
+    }
+}
--- BulkyBookWeb/Areas/Admin/Controllers/DashboardController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/DashboardController.cs
@@ -1,6 +1,9 @@
-﻿using BulkyBook.Utility;
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.Models.ViewModels;
+using BulkyBook.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyBookWeb.Areas.Admin.Controllers
 {
@@ -8,9 +11,50 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
     [Authorize(Roles = SD.RoleAdmin)]
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _db;
+        public DashboardController(ApplicationDbContext db)
         {
-            return View();
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var orders = await _db.OrderHeaders.ToListAsync();
+            var productCount = await _db.Products.CountAsync();
+            var usersCount = await _db.ApplicationUsers.CountAsync();
+            DashboardVM dashboardVM = new()
+            {
+                TotalOrders = orders.Count,
+                TotalProducts = productCount,
+                TotalUsers = usersCount,
+                TotalRevenue = orders.Where(o => o.OrderStatus == SD.StatusApproved || o.OrderStatus == SD.StatusShipped).Sum(o => o.OrderTotal)
+            };
+            return View(dashboardVM);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetChartData()
+        {
+            var orders = await _db.OrderHeaders.ToListAsync();
+            var products = await _db.Products.Include(u => u.Category).ToListAsync();
+            var categories = await _db.Categories.ToListAsync();
+
+            //Revenue by month (last 6 months)
+            var now = DateTime.UtcNow;
+            var sixMonthAge = now.AddMonths(-5);
+            var monthlyRevenue = Enumerable.Range(0, 6).Select(i =>
+            {
+                var month = sixMonthAge.AddMonths(i);
+                var revenue = orders.Where(o => o.OrderDate.Year == month.Year && o.OrderDate.Month == month.Month
+                && (o.OrderStatus == SD.StatusApproved || o.OrderStatus == SD.StatusShipped)).Sum(o => o.OrderTotal);
+
+                return new { Label = month.ToString("MMM yyyy"), Revenue = revenue };
+            });
+
+            return Json(new
+            {
+                monthlyRevenue
+            });
         }
     }
 }
